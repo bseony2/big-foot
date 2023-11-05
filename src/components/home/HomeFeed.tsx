670ca9514d640036ec41d765709e1a8df6650b9d
nbbeom <5877177a@naver.com>
2023-11-05 21:54:27 +0900

    FR-01/ 케밥 케이스 제거 #1

--- src/components/home/HomeFeed.tsx
+++ src/components/home/HomeFeed.tsx
@@ -60,7 +60,7 @@ function HomeContainer(): ReactElement {
                           className="w-4 h-4 ml-2"
                           viewBox="0 0 24 24"
                           stroke="currentColor"
-                          stroke-width="2"
+                          strokeWidth="2"
                           fill="none"
                           stroke-linecap="round"
                           stroke-linejoin="round"
@@ -73,7 +73,7 @@ function HomeContainer(): ReactElement {
                         <svg
                           className="w-4 h-4 mr-1"
                           stroke="currentColor"
-                          stroke-width="2"
+                          strokeWidth="2"
                           fill="none"
                           stroke-linecap="round"
                           stroke-linejoin="round"
@@ -88,7 +88,7 @@ function HomeContainer(): ReactElement {
                         <svg
                           className="w-4 h-4 mr-1"
                           stroke="currentColor"
-                          stroke-width="2"
+                          strokeWidth="2"
                           fill="none"
                           stroke-linecap="round"
                           stroke-linejoin="round"
