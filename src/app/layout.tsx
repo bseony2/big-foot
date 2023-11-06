2e859cfbf9ce5394180aefb3339a4c65ab3461db
nbbeom <40671240+nbbeom@users.noreply.github.com>
2023-11-06 20:51:54 +0900

    FR-01/ left side bar 구성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import LeftSidebar from "components/LeftSidebar";
+import LeftSidebar from "components/layouts/LeftSidebar";
 
 
 const inter = Inter({ subsets: ["latin"] });
